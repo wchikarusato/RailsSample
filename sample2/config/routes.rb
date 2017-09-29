024c2a2310ed75e54fdf6a6033ce394f50176b0e
Hikaru Sato <hikaru.sato@webcrew.co.jp>
2017-09-29 16:07:34 +0900

    リソースベースのルーティング設定 まで

--- sample2/config/routes.rb
+++ sample2/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :blogs
   get 'disp(/:type(/:price))' => 'books#show', as: 'book_show'
   post 'info/index'
   get 'index' => 'info#index', as: 'gindex'
