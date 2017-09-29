a1809c2d329306f5b46b685605eb45427a2e6605
Hikaru Sato <hikaru.sato@webcrew.co.jp>
2017-09-29 15:42:58 +0900

    matchメソッドを使ったルーティング設定 まで

--- sample2/config/routes.rb
+++ sample2/config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/show'
-
-  get 'info/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # /controller/actionのルーティング
-  get ':controller(/:action(/:id))(.:format)'
-  root :to => 'info#index'
+  get 'disp(/:type(/:price))' => 'books#show', as: 'book_show'
+  post 'info/index'
+  get 'index' => 'info#index', as: 'gindex'
 end
