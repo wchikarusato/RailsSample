5914bd6c4694d90d569a22b5be2cbc0008150168
Hikaru Sato <hikaru.sato@webcrew.co.jp>
2017-09-29 15:32:20 +0900

    以前に使われていたルーティング まで

--- sample2/config/routes.rb
+++ sample2/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'books/show'
+
   get 'info/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # /controller/actionのルーティング
   get ':controller(/:action(/:id))(.:format)'
   root :to => 'info#index'
 end
